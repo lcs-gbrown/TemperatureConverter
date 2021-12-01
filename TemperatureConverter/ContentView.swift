9e05722981607ad01149f03e4a8a4a63aa7f54f4
gabi brown <gbrown@lcs.on.ca>
2021-11-30 19:54:35 -0500

    added feedback

--- TemperatureConverter/ContentView.swift
+++ TemperatureConverter/ContentView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @State var feedback = ""
+    
     @State var celsius: Double = 10.0
     
     //MARK: Computed properties
@@ -18,7 +20,7 @@ struct ContentView: View {
     
     var body: some View {
         VStack(alignment: .leading) {
-          
+            
             //Input
             Text("Temperature in Celsius:")
                 .bold()
@@ -35,26 +37,57 @@ struct ContentView: View {
                 }
                 //The syntax of $ says to use the property radius, and BIND it to this control
                 //This value means when the control changes, the property's value changes
-               Slider(value: $celsius,
-                       in: 0.0...100.0,
+                Slider(value: $celsius,
+                       in: -50.0...50.0,
                        label: {
-                            Text("temperature")
+                    Text("temperature")
                 },
                        minimumValueLabel: {
-                    Text("0.0")
+                    Text("-50.0")
                 },
                        maximumValueLabel: {
                     Text("100.0")
                 })
-
+                
+                Button(action: {
+                    
+                    //Create a temporary constant witha gess as an integer
+                    let celsiusInteger = Int(celsius)
+                    
+                    //Check the users guess
+                    if celsiusInteger > 50 {
+                    feedback = "It's boiling. Drink lots of water and wear sunscreen"
+                } else if celsiusInteger > 30 {
+                    feedback = "It's balmy! Tee-shirt and shorts weather!"
+                } else if celsiusInteger > 10 {
+                    feedback = "Just wear a long sleeve"
+                } else if celsiusInteger < 9 {
+                    feedback = "It's chilly. Wear a little jacket."
+                } else if celsiusInteger < 0 {
+                    feedback = "It's cold. Dress warm."
+                } else if celsiusInteger < 20 {
+                    feedback = "It's freezing. Lots of layers!"
+                }
+                       
+                       print("Button was pressed")
+                       }, label: {
+                    Text("Submit Temperature")
+                })
+                    .buttonStyle(.bordered)
+                
+                
+                
+                Text("\(feedback)")
+                    .italic()
+                    .padding()
                 
             }
-
+            
             
             
             //Output
             Text("Fahrenheit")
-                    .bold()
+                .bold()
             
             Text("\(String(format:"%.1f",fahrenheit))")
             
